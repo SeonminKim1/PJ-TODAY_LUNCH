deea3efb4886d48e44026d6565bbb018f3ee51a2
SeonminKim1 <yubi5050@naver.com>
2022-06-04 03:12:44 +0900

    Login & Join #1 #3

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class UserModel(AbstractUser):
+    # address, gender, birthdate
+    address = models.CharField(max_length=256)
+    gender = models.CharField(max_length=10)
+    birthdate = models.DateField(auto_now=False)
--- users/views.py
+++ users/views.py
@@ -1,81 +1,67 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from .models import UserModel
-from django.contrib.auth import get_user_model #사용자가 있는지 검사하는 함수
-
-#user/views.py
-
+from django.contrib import auth # 사용자 auth 기능
 from django.contrib.auth.decorators import login_required
+
+from datetime import datetime
+
+# Join
+def join_view(request):
+    # Page View
+    if request.method == 'GET':
+        return render(request, 'users/join.html')
+
+    # Page POST
+    elif request.method == 'POST':
+        user_email = request.POST.get('user_email', ' ')
+        user_name = request.POST.get('user_name', ' ')
+        password = request.POST.get('password', ' ')
+        address = request.POST.get('address', ' ')
+        gender = request.POST.get('gender', ' ')
+        birthdate = request.POST.get('birthdate', ' ')
+        
+        date_format = '%y%m%d'
+        birthdate = datetime.strptime(birthdate, date_format).date()        
+        
+        print(user_email, user_name, password, address, gender, birthdate)
+        user_model = UserModel()
+        user_model.email = user_email
+        user_model.username = user_name
+        user_model.password = password
+        user_model.address = address
+        user_model.gender = gender
+        user_model.birthdate = birthdate
+        user_model.save()
+
+        return redirect('login')  # 회원가입이 완료되었으므로 로그인 페이지로 이동
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('user_name', "")
+        password = request.POST.get('password', "")
+
+        # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
+        me = auth.authenticate(request, username=username, password=password)
+        if me is not None:  
+            auth.login(request, me)
+            return redirect('main')
+        else:
+            return render(request,'users/login.html')  # 로그인 실패
+
+    elif request.method == 'GET':
+        user = request.user.is_authenticated  # 사용자가 로그인 되어 있는지 검사
+        if user:  # 로그인이 되어 있다면
+            return redirect('main')
+        else:  # 로그인이 되어 있지 않다면
+            return render(request, 'users/login.html')
 
 @login_required # 로그인 한 사용자만 함수 호출 가능
 def logout(request):
     auth.logout(request) # 인증 되어있는 정보를 없애기
     return redirect("/")
 
+def init_view(request):
+    return render(request, 'init/init.html')
 
-def sign_up_view(request):
-    if request.method == 'GET':
-        user = request.user.is_authenticated  # 로그인 된 사용자가 요청하는지 검사
-        if user:  # 로그인이 되어있다면
-            return redirect('/')
-        else:  # 로그인이 되어있지 않다면
-            return render(request, 'user/signup.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        password2 = request.POST.get('password2', '')
-        bio = request.POST.get('bio', '')
-
-        if password != password2:
-            # 패스워드가 다르다는 에러가 필요합니다. {'error':'에러문구'} 를 만들어서 전달합니다.
-            return render(request, 'user/signup.html', {'error': '패스워드를 확인 해 주세요!'})
-        else:
-            if username == '' or password == '':
-                # 사용자 저장을 위한 username과 password가 필수라는 것을 얘기 해 줍니다.
-                return render(request, 'user/signup.html', {'error': '사용자 이름과 패스워드는 필수 값 입니다'})
-
-            exist_user = get_user_model().objects.filter(username=username)
-            if exist_user:
-                return render(request, 'user/signup.html', {'error':'사용자가 존재합니다.'})  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
-            else:
-                UserModel.objects.create_user(username=username, password=password, bio=bio)
-                return redirect('/sign-in')  # 회원가입이 완료되었으므로 로그인 페이지로 이동
-
-
-from django.contrib import auth # 사용자 auth 기능
-
-def sign_in_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', "")
-        password = request.POST.get('password', "")
-
-        me = auth.authenticate(request, username=username, password=password)  # 사용자 불러오기
-        if me is not None:  # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
-            auth.login(request, me)
-            return redirect('/')
-        else:
-            return render(request,'user/signin.html',{'error':'유저이름 혹은 패스워드를 확인 해 주세요'})  # 로그인 실패
-    elif request.method == 'GET':
-        user = request.user.is_authenticated  # 사용자가 로그인 되어 있는지 검사
-        if user:  # 로그인이 되어 있다면
-            return redirect('/')
-        else:  # 로그인이 되어 있지 않다면
-            return render(request, 'user/signin.html')
-# user/views.py 
-
-@login_required
-def user_view(request):
-    if request.method == 'GET':
-        # 사용자를 불러오기, exclude와 request.user.username 를 사용해서 '로그인 한 사용자'를 제외하기
-        user_list = UserModel.objects.all().exclude(username=request.user.username)
-        return render(request, 'user/user_list.html', {'user_list': user_list})
-
-
-@login_required
-def user_follow(request, id):
-    me = request.user
-    click_user = UserModel.objects.get(id=id)
-    if me in click_user.followee.all():
-        click_user.followee.remove(request.user)
-    else:
-        click_user.followee.add(request.user)
-    return redirect('/user')
+def main_view(request):
+    return render(request, 'main/main.html')
