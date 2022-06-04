d659e01ab6236558728d0dbe834beb35b23726fd
SeonminKim1 <yubi5050@naver.com>
2022-06-04 19:02:14 +0900

    Validation Code Final Check #1 #3

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class UserModel(AbstractUser):
     class Meta:
-        db_table = "USERS"
+        db_table = "USER"
 
     def __str__(self):
         return self.username
@@ -15,11 +15,11 @@ class UserModel(AbstractUser):
     )
 
     # address, gender, birthdate
+    fullname = models.CharField(max_length=10, unique=False, null=False, verbose_name='이름')
     address = models.CharField(max_length=256, null=False, verbose_name='주소')
     gender = models.CharField(max_length=10, choices=GENDER_CHOICE, verbose_name='성별')
     birthdate = models.DateField(auto_now=False, verbose_name='생년월일')
 
     # email unique 필드로 정의하여 유저네임필드로 지정
-    email = models.EmailField(max_length=254, unique=True, verbose_name='email')
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    # email = models.EmailField(max_length=254, unique=True, verbose_name='email')
+    # REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.contrib import auth  # 사용자 auth 기능
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
@@ -22,70 +23,81 @@ def join_view(request):
 
     # Page POST
     elif request.method == 'POST':
-        user_email = request.POST.get('user_email', ' ')
-        user_name = request.POST.get('user_name', ' ')
-        password = request.POST.get('password', ' ')
+        user_email = request.POST.get('user_email', '')
+        user_name = request.POST.get('user_name', '')
+        password = request.POST.get('password', '')
         password2 = request.POST.get('password2', '')
-        address = request.POST.get('address', ' ')
-        address_detail = request.POST.get('address_detail', ' ')
+        address = request.POST.get('address', '')
+        address_detail = request.POST.get('address_detail', '')
 
-        gender = request.POST.get('gender', ' ')
-        birthdate = request.POST.get('birthdate', ' ')
+        gender = request.POST.get('gender', '') # 'M' , 'W'
+        birthdate = request.POST.get('birthdate', '') # 940526
 
+        # validation 1 ) user email, password 잘 들어왔는지.
+        if user_email == '' or password == '' or password2 == '':
+            messages.error(request, '사용자 아이디와 패스워드는 필수 값 입니다.')
+            return render(request, 'users/join.html', {'error': '사용자 아이디와 패스워드는 필수 값 입니다'})
+
+        # validation 2 ) 가입된 이메일인지
+        exist_user = get_user_model().objects.filter(email=user_email)
+        if exist_user:
+            messages.error(request, '이미 가입된 이메일 입니다.')
+            return render(request, 'users/join.html', {'error': '이미 가입된 이메일입니다.'})
+        
+        # validation 3 ) password 중복체크
+        if password != password2:
+            messages.error(request, '패스워드가 서로 다릅니다. 패스워드를 확인해 주세요!')
+            return render(request, 'users/join.html', {'error': '패스워드를 확인 해 주세요!'})
+
+        # validation 4) 생년월일 6자리인지
+        if len(birthdate.strip()) != 6:
+            messages.error(request, '생년월일을 형식에 맞춰 작성해 주세요!')
+            return render(request, 'users/join.html', {'error': '생년월일을 형식에 맞춰 작성해 주세요!'})
+
+        # Process Data
         final_address = address + ', ' + address_detail
         birthdate = datetime.strptime(birthdate, '%y%m%d').date()
+
         print(user_email, user_name, password, final_address, gender, birthdate)
+        # 모든 validation 통과~
+        UserModel.objects.create_user(
+            email=user_email, username=user_email, full_name = user_name,
+            password=password, address=final_address,
+            gender=gender, birthdate=birthdate)
 
-        if password != password2:
-            return render(request, 'users/join.html', {'error': '패스워드를 확인 해 주세요!'})
-        else:
-            if user_email == '' or password == '':
-                # 사용자 저장을 위한 username과 password가 필수라는 것을 얘기 해 줍니다.
-                return render(request, 'users/join.html', {'error': '사용자 아이디와 패스워드는 필수 값 입니다'})
-
-            exist_user = get_user_model().objects.filter(email=user_email)
-            if exist_user:
-                return render(request, 'users/join.html',
-                              {'error': '이미 가입된 이메일입니다.'})  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
-            else:
-                UserModel.objects.create_user(
-                    email=user_email, username=user_name,
-                    password=password, address=final_address,
-                    gender=gender, birthdate=birthdate)
-                print('회원가입이 정상적으로 완료 되었습니다~')
-                return redirect('login')  # 회원가입이 완료되었으므로 로그인 페이지로 이동
+        messages.success(request, '회원가입이 정상적으로 완료 되었습니다')
+        print('회원가입이 정상적으로 완료 되었습니다~')
+        return redirect('login')  # 회원가입이 완료되었으므로 로그인 페이지로 이동
 
 
 def login_view(request):
-    if request.method == 'POST':
-        user_email = request.POST.get('user_email', "")
-        password = request.POST.get('password', "")
-
-        print('===', user_email, password)
-        # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
-        # 여기 auth.authenticate 제대로 작동안하는 오류있습니다.
-        me = auth.authenticate(request, email=user_email, password=password)
-        print('me', me)
-        if me is not None:
-            auth.login(request, me)
-            return redirect('main')
-        else:
-            print(1)
-            return render(request, 'users/login.html', {'error': '유저이름 혹은 패스워드를 확인 해 주세요'})  # 로그인 실패
-
-    elif request.method == 'GET':
+    # Login page
+    if request.method == 'GET':
         user = request.user.is_authenticated  # 사용자가 로그인 되어 있는지 검사
         if user:  # 로그인이 되어 있다면
             return redirect('main')
         else:  # 로그인이 되어 있지 않다면
-            print(2)
-
             return render(request, 'users/login.html')
+    # Login Button - Click
+    elif request.method == 'POST':
+        user_email = request.POST.get('user_email', "")
+        password = request.POST.get('password', "")
+        # print('===', user_email, password)
+        
+        # validation 1) username과 password 비교
+        me = auth.authenticate(request, username=user_email, password=password) # me 정상시 username이 됨.
+        if me is not None:
+            auth.login(request, me)
+            return redirect('main')
+        else: # 로그인 인증 실패
+            messages.error(request, '로그인 실패! 아이디 or 패스워드를 확인 해 주세요!')
+            return render(request, 'users/login.html', {'error': '로그인 실패! 아이디 or 패스워드를 확인 해 주세요!'})  # 로그인 실패
 
 
 @login_required  # 로그인 한 사용자만 함수 호출 가능
 def logout(request):
-    auth.logout(request)  # 인증 되어있는 정보를 없애기
+    auth.logout(request) # 인증 되어있는 정보를 없애기
+    messages.success(request, '로그아웃 성공!')
     return redirect("/")
 
 
