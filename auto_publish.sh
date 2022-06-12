1621cd70352392a672bb4ebc38bb2f0326ed02ba
SeonminKim1 <yubi5050@naver.com>
2022-06-12 10:59:34 +0000

    Update .sh auto publish

--- auto_publish.sh
+++ auto_publish.sh
@@ -6,7 +6,10 @@ echo "==== makemigrations, migrate 완료"
 
 # sudo python3 auto_change_img-path.py
 
-echo "==== Change img-path 완료"
+# echo "==== Change img-path 완료"
+
+ln -s /mnt/luckyseven-s3/img/ static/
+echo "==== img mnt 바로가기 생성 완료"
 
 python3 recommandation/db_uploader.py
 echo "==== Restaurant DB Upload 완료"
